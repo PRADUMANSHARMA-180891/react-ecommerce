55e2241ab1731389e672c2a493628553a63ce2ef
PRADUMANSHARMA-180891 <praduman.cuh@gmail.com>
2024-06-22 17:09:18 +0530

    added cart page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
 import Home from "./page/homepage/Home"
 
+import './App.css';
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from 'react-router-dom';
+import LoginPage from "./page/authpage/LoginPage";
+import SignupPage from "./page/authpage/SignupPage";
+import CartPage from "./page/cartpage/CartPage";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+  },
+  {
+    path: '/login',
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: '/signup',
+    element: <SignupPage></SignupPage>,
+  },
+  {
+    path: '/cart',
+    element: <CartPage></CartPage>
+  },
+]);
+
 function App() {
-  
   return (
-    <>
-         <Home />   
-    </>
-  )
+    <div className="App">
+      {/* <Home></Home> */}
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
-export default App
+
+export default App;
---
+++ src/page/authpage/LoginPage.jsx
@@ -0,0 +1,9 @@
+import Login from "../../feature/auth/loginpage/Login";
+
+function LoginPage() {
+    return ( <div>
+        <Login></Login>
+    </div>  );
+}
+
+export default LoginPage;
